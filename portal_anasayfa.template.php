552a15730abfcb62dbd52d217f133446fe443a5c
CeeMoo <qdale.123@gmail.com>
2022-04-03 20:22:48 +0300

    up

--- portal_anasayfa.template.php
+++ portal_anasayfa.template.php
@@ -2,5 +2,19 @@
 
 function template_main()
 {
-	echo 'merhaba';
+?>
+
+<div class="card">
+  <div class="card-header">
+    Bir Hadis
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>Kolaylaştırınız! Zorlaştırmayınız! Müjdeleyiniz, nefret ettirmeyiniz! Birbirinizle anlaşın, iyi geçinin, ihtilâfa düşmeyin!"</p>
+      <footer class="blockquote-footer">(Buharî, 3:72)</footer>
+    </blockquote>
+  </div>
+</div>
+
+<?php
 }
